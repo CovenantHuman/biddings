ca5dc1a6576d0354828c49b27d422e3b582cd5f2
Michaela Healton <mshealton@gmail.com>
2023-03-06 17:41:39 -0800

    Add test for To Do Item Model

--- test/models/to_do_item_test.rb
+++ test/models/to_do_item_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class ToDoItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create a to do item" do
+    to_do_item = ToDoItem.new(to_do_list: to_do_lists(:list_one), completed: true, name: "laundry")
+    assert to_do_item.save
+    saved_to_do_item = ToDoItem.find_by(name: "laundry")
+    assert saved_to_do_item
+    assert saved_to_do_item.completed
+    assert_equal "list_one", saved_to_do_item.to_do_list.name
+  end
 end
