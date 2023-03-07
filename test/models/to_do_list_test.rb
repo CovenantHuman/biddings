175fd98cd73ecfd6abc9b265e7a741b631fb8f77
Michaela Healton <mshealton@gmail.com>
2023-03-06 17:34:38 -0800

    Add test for To Do List Model

--- test/models/to_do_list_test.rb
+++ test/models/to_do_list_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class ToDoListTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create a to do list" do
+    to_do_list = ToDoList.new(name: "test", giver: users(:user_one), recipient: users(:user_two), active: true)
+    assert to_do_list.save
+    saved_to_do_list = ToDoList.find_by(name: "test")
+    assert saved_to_do_list
+    assert saved_to_do_list.active
+    assert_equal "one", saved_to_do_list.giver.name
+    assert_equal "two", saved_to_do_list.recipient.name
+  end
 end
