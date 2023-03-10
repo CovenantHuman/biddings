4239368f435c269c70239caa04d8e5c852422f15
Michaela Healton <mshealton@gmail.com>
2023-03-09 17:12:27 -0800

    Change Column Name in User Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_05_001949) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_10_005657) do
   create_table "to_do_items", force: :cascade do |t|
     t.integer "to_do_list_id", null: false
     t.boolean "completed"
@@ -33,7 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_05_001949) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
