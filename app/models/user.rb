0c614964bba07eea4787f186175d0220f4a9a811
Michaela Healton <mshealton@gmail.com>
2023-03-15 14:21:10 -0700

    Add has_secure_password to model and change tests to use it instead of setting password_digest directly where appropriate.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     before_validation :downcase_email
 
+    has_secure_password
+
     has_many :given_to_do_lists, class_name: "ToDoList", inverse_of: :giver, foreign_key: :giver_id
     has_many :received_to_do_lists, class_name: "ToDoList", inverse_of: :recipient, foreign_key: :recipient_id
     validates :email, presence: true, format: {with: URI::MailTo::EMAIL_REGEXP}, uniqueness: true
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   test "create a user" do
-    user = User.new(email: "user@example.com", name: "user", password_digest: "test")
+    user = User.new(email: "user@example.com", name: "user", password: "test", password_confirmation: "test")
     assert user.save
     saved_user = User.find_by email: "user@example.com"
     assert saved_user
@@ -10,14 +10,14 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "destroy a user" do
-    user = User.new(email: "user@example.com", name: "user", password_digest: "test")
+    user = User.new(email: "user@example.com", name: "user", password: "test", password_confirmation: "test")
     user.save
     assert user.destroy
     assert_not User.find_by(email: "user@example.com")
   end
 
   test "update name and email fields" do
-    user = User.new(email: "user@example.com", name: "user", password_digest: "test")
+    user = User.new(email: "user@example.com", name: "user", password: "test", password_confirmation: "test")
     user.save
     saved_user = User.find_by email: "user@example.com"
     saved_user.email = "updated_user@example.com"
@@ -29,7 +29,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "read name and email fields" do
-    user = User.new(email: "user@example.com", name: "user", password_digest: "test")
+    user = User.new(email: "user@example.com", name: "user", password: "test", password_confirmation: "test")
     user.save
     saved_user = User.find_by email: "user@example.com"
     assert_equal "user@example.com", saved_user.email
@@ -37,17 +37,17 @@ class UserTest < ActiveSupport::TestCase
   end 
 
   test "emails are required to be in a certain format" do
-    user_with_bad_email = User.new(email: "userexamplecom", name: "user", password_digest: "test")
+    user_with_bad_email = User.new(email: "userexamplecom", name: "user", password: "test", password_confirmation: "test")
     assert_not user_with_bad_email.save
   end
 
-  test "password_digests must follow certain paramaters" do
+  test "passwords must follow certain paramaters" do
     skip("This functionality is not yet implemented.")
-    user_with_bad_password_digest = User.new(email: "user@example.com", name: "user", password_digest: "")
-    assert_not user_with_bad_password_digest.save
+    user_with_bad_password = User.new(email: "user@example.com", name: "user", password: "", password_confirmation: "")
+    assert_not user_with_bad_password.save
   end
 
-  test "password_digest can be validated or not appropriately" do
+  test "password can be validated or not appropriately" do
     skip("This functionality is not yet implemented.")
     flunk
   end
@@ -81,32 +81,32 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "emails should be unique" do
-    user = User.new(email: "user@example.com", name: "user", password_digest: "test")
+    user = User.new(email: "user@example.com", name: "user", password: "test", password_confirmation: "test")
     user.save
-    second_user = User.new(email: "user@example.com", name: "second_user", password_digest: "test")
+    second_user = User.new(email: "user@example.com", name: "second_user", password: "test", password_confirmation: "test")
     assert_not second_user.save
   end
 
-  test "attempting to read password_digest fails" do
+  test "attempting to read password fails" do
     skip("This functionality is not yet implemented.")
-    user = User.new(email: "user@example.com", name: "user", password_digest: "test")
+    user = User.new(email: "user@example.com", name: "user", password: "test", password_confirmation: "test")
     user.save
     saved_user = User.find_by email: "user@example.com"
-    assert_not saved_user.password_digest
+    assert_not saved_user.password
   end
  
-  test "password_digest can be updated" do
+  test "password can be updated" do
     skip("This functionality is not yet implemented.")
-    user = User.new(email: "user@example.com", name: "user", password_digest: "test")
+    user = User.new(email: "user@example.com", name: "user", password: "test", password_confirmation: "test")
     user.save
     saved_user = User.find_by email: "user@example.com"
     flunk
   end
 
   test "email should be case insensitive" do
-    user = User.new(email: "user@example.com", name: "user", password_digest: "test")
+    user = User.new(email: "user@example.com", name: "user", password: "test", password_confirmation: "test")
     user.save
-    user_should_fail = User.new(email: "USER@example.com", name: "user", password_digest: "test")
+    user_should_fail = User.new(email: "User@example.com", name: "user", password: "test", password_confirmation: "test")
     assert_not user_should_fail.save
   end
 end
