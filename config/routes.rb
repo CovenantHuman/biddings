aeb1bc32ff9fee11b3e38c1d6e9f61962719cf39
Michaela Healton <mshealton@gmail.com>
2023-04-17 17:30:04 -0700

    Build confirmation page and update routes accordingly

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   post 'sign_up', to: 'users#create'
   get 'sign_up', to: 'users#new'
+  resources :confirmations, only: [:create, :edit, :new], param: :confirmation_token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
