5b5b94b23082552991cfab586bf2156f776c319e
Michaela Healton <mshealton@gmail.com>
2023-04-24 14:08:16 -0700

    Add session controller, routes, and view to allow for login

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   post 'sign_up', to: 'users#create'
   get 'sign_up', to: 'users#new'
   resources :confirmations, only: [:create, :edit, :new], param: :confirmation_token
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  get "login", to: "sessions#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
