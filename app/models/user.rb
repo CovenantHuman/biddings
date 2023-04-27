f692fa64d82bd3b079739acca1a7f7e23e5b73e5
Michaela Healton <mshealton@gmail.com>
2023-04-26 17:23:48 -0700

    Add unconfirmed email functionality to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
     MAILER_FROM_EMAIL = "no-reply@example.com"
     PASSWORD_RESET_TOKEN_EXPIRATION = 10.minutes
 
+    attr_accessor :current_password
+
+    before_save :downcase_unconfirmed_email
     before_validation :downcase_email
 
     has_secure_password
@@ -11,9 +14,18 @@ class User < ApplicationRecord
     has_many :received_to_do_lists, class_name: "ToDoList", inverse_of: :recipient, foreign_key: :recipient_id
     validates :email, presence: true, format: {with: URI::MailTo::EMAIL_REGEXP}, uniqueness: true
     validates :password_digest, presence: true
+    validates :unconfirmed_email, format: {with: URI::MailTo::EMAIL_REGEXP, allow_blank: true}
+
 
     def confirm!
-        update_columns(confirmed_at: Time.current)
+        if unconfirmed_or_reconfirming?
+            if unconfirmed_email.present?
+                return false unless update(email: unconfirmed_email, unconfirmed_email: nil)
+            end
+            update columns(confirmed_at: Time.current)
+        else
+            false
+        end 
     end
 
     def confirmed?
@@ -41,7 +53,23 @@ class User < ApplicationRecord
         password_reset_token = generate_password_reset_token
         UserMailer.password_reset(self, password_reset_token).deliver_now
     end
-    
+
+    def confirmable_email
+        if unconfirmed_email.present?
+            unconfirmed_email
+        else
+            email
+        end    
+    end
+
+    def reconfirming?
+        unconfirmed_email.present?
+    end
+
+    def unconfirmed_or_reconfirming?
+        unconfirmed? || reconfirming?
+    end
+
     private
 
     def downcase_email
@@ -50,5 +78,10 @@ class User < ApplicationRecord
         end
         # also possible to write as:
         # self.email = email&.downcase.strip
+    end
+
+    def downcase_unconfirmed_email
+        return if unconfirmed_email.nil?
+        self.unconfirmed_email = unconfirmed_email.downcase.strip
     end
 end
