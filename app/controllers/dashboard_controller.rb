0c6096f380dbf45c9bcc4c37e3644459e156812f
Michaela Healton <mshealton@gmail.com>
2023-05-08 16:04:35 -0700

    Add pending invitations to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
     before_action :authenticate_user!
 
     def show
+        @invitations = ToDoListInvite.where(invitee_email: current_user.email, accepted_at: nil).all
     end
 end
