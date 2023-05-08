238112c6c638cd1bac1a04c546e74a69fc469ca6
Michaela Healton <mshealton@gmail.com>
2023-05-08 15:33:20 -0700

    Downcase invitee email before saving it

--- app/models/to_do_list_invite.rb
+++ app/models/to_do_list_invite.rb
@@ -4,11 +4,19 @@ class ToDoListInvite < ApplicationRecord
     belongs_to :giver, class_name: "User", optional: true
     belongs_to :recipient, class_name: "User", optional: true
 
+    before_save :downcase_invitee_email
+
     def accepted?
         to_do_list != null
     end
 
     def send_to_do_list_invitation_email!
         ToDoListInviteMailer.invitation(self).deliver_now
+    end
+
+    private
+
+    def downcase_invitee_email
+        self.invitee_email = invitee_email&.downcase.strip
     end
 end
