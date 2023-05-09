121e101f4909ff3be3740e60d1f0daf3329a427c
Michaela Healton <mshealton@gmail.com>
2023-05-09 15:58:44 -0700

    Made it possible to accept invitations and display the created to do lists

--- app/controllers/to_do_list_invites_controller.rb
+++ app/controllers/to_do_list_invites_controller.rb
@@ -15,6 +15,30 @@ class ToDoListInvitesController < ApplicationController
         @to_do_list_invite = ToDoListInvite.new
     end
 
+    def accept
+        @invitation = ToDoListInvite.find(params[:to_do_list_invite_id])
+        @invitation.accepted_at = DateTime.now
+        if @invitation.save
+            @invitee = User.find_by(email: @invitation.invitee_email)
+            @inviter = User.find(@invitation.inviter_id)
+            @to_do_list_given_by_intivee = ToDoList.new(name: "For #{@inviter.name} to do", 
+                                                        giver_id: @invitee.id,
+                                                        recipient_id: @invitation.inviter_id,
+                                                        active: true,
+                                                        to_do_list_invite_id: @invitation.id)
+            @to_do_list_given_by_inviter = ToDoList.new(name: "To dos from #{@inviter.name}",
+                                                         giver_id: @invitation.inviter_id,
+                                                         recipient_id: @invitee.id,
+                                                         active: true,
+                                                         to_do_list_invite_id: @invitation.id)
+            @to_do_list_given_by_intivee.save
+            @to_do_list_given_by_inviter.save
+            redirect_to dashboard_path, notice: "Invitation accepted!"
+        else
+            redirect_to dashboard_path, status: :unprocessable_entity
+        end
+    end
+
     private
 
     def to_do_list_invite_params
