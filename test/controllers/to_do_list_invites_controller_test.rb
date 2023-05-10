c62a3b43c5fd0c524facc5caad4f7c337d4d0c5f
Michaela Healton <mshealton@gmail.com>
2023-05-09 17:10:20 -0700

    Fix password_digest in fixtures to actually contain a password_digest and create a helper method to signin test users

--- test/controllers/to_do_list_invites_controller_test.rb
+++ test/controllers/to_do_list_invites_controller_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class ToDoListInvitesControllerTest < ActionDispatch::IntegrationTest
-  test "the truth" do
-    assert true
+  test "new action should bounce a non logged in user to the homepage" do
+    get "/to_do_list_invites/new"
+    assert_redirected_to login_path
   end
 end
