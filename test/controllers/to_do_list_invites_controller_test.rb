b4b05bdb8ff171a6adf036665767e13d0a1aa7aa
Michaela Healton <mshealton@gmail.com>
2023-05-11 13:52:51 -0700

    Test invitation acceptance

--- test/controllers/to_do_list_invites_controller_test.rb
+++ test/controllers/to_do_list_invites_controller_test.rb
@@ -18,4 +18,22 @@ class ToDoListInvitesControllerTest < ActionDispatch::IntegrationTest
     assert_equal ["two@example.com"], mail.to
     assert_match "invited", mail.body.encoded
   end
+
+  test "create and accept an invitation" do
+    inviter = users(:user_one)
+    invitee = users(:user_two)
+
+    sign_in_as inviter
+    post to_do_list_invites_path(to_do_list_invite:{invitee_email: invitee.email})
+    delete logout_path
+
+    sign_in_as invitee
+    invite = ToDoListInvite.find_by(inviter_id: users(:user_one).id) 
+    post to_do_list_invite_accept_path(to_do_list_invite_id: invite.id) 
+    assert_redirected_to dashboard_path
+    assert_equal "Invitation accepted!", flash[:notice]
+    assert(ToDoList.find_by(to_do_list_invite_id: invite.id, giver_id: invitee.id, recipient_id: inviter.id))
+    assert(ToDoList.find_by(to_do_list_invite_id: invite.id, giver_id: inviter.id, recipient_id: invitee.id))
+  end
 end
+
