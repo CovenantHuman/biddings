57f07b82b02180eee93455758ba3dc229c5f611d
Michaela Healton <mshealton@gmail.com>
2023-05-12 14:18:20 -0700

    Remove references to to do lists being active from dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
 
     def show
         @invitations = ToDoListInvite.where(invitee_email: current_user.email, accepted_at: nil).all
-        @to_do_lists_given = ToDoList.where(giver_id: current_user.id, active: true).all
-        @to_do_lists_received = ToDoList.where(recipient_id: current_user.id, active: true).all
+        @to_do_lists_given = ToDoList.where(giver_id: current_user.id).all
+        @to_do_lists_received = ToDoList.where(recipient_id: current_user.id).all
     end
 end
