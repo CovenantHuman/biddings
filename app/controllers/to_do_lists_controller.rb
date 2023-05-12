87e153fe0add322aeb6e3e4a8e8f32e50fb41927
Michaela Healton <mshealton@gmail.com>
2023-05-12 15:19:52 -0700

    Start defining show action in to do lists controller

--- app/controllers/to_do_lists_controller.rb
+++ app/controllers/to_do_lists_controller.rb
@@ -1,2 +1,9 @@
-class ToDoListController < ApplicationController
+class ToDoListsController < ApplicationController
+    before_action :authenticate_user!
+
+    def show
+        @to_do_list = ToDoList.find(params[:id])
+        # I18n.config.available_locales = [:giver, :recipient]
+        # I18n.locale = current_user.id == @to_do_list.giver_id ? :giver : :recipient
+    end
 end
