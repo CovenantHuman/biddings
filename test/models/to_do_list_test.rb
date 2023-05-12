92ac8ecb9b5535da7ac8f6968d181f5f0d882b75
Michaela Healton <mshealton@gmail.com>
2023-05-11 17:57:17 -0700

    Update tests and fixtures to reflect changes in database

--- test/models/to_do_list_test.rb
+++ test/models/to_do_list_test.rb
@@ -2,11 +2,10 @@ require "test_helper"
 
 class ToDoListTest < ActiveSupport::TestCase
   test "create a to do list" do
-    to_do_list = ToDoList.new(name: "test", giver: users(:user_one), recipient: users(:user_two), active: true)
+    to_do_list = ToDoList.new(name: "test", giver: users(:user_one), recipient: users(:user_two))
     assert to_do_list.save
     saved_to_do_list = ToDoList.find_by(name: "test")
     assert saved_to_do_list
-    assert saved_to_do_list.active
     assert_equal "one", saved_to_do_list.giver.name
     assert_equal "two", saved_to_do_list.recipient.name
   end
@@ -16,32 +15,26 @@ class ToDoListTest < ActiveSupport::TestCase
     assert_raises(ActiveRecord::NotNullViolation) do 
       to_do_list.save(validate: false)
     end
-    to_do_list = ToDoList.new(giver: nil, recipient: users(:user_two), active: true)
+    to_do_list = ToDoList.new(giver: nil, recipient: users(:user_two))
     assert_raises(ActiveRecord::NotNullViolation) do
       to_do_list.save(validate: false)
     end
-    to_do_list = ToDoList.new(giver: users(:user_one), recipient: nil, active: true)
+    to_do_list = ToDoList.new(giver: users(:user_one), recipient: nil)
     assert_raises(ActiveRecord::NotNullViolation) do 
       to_do_list.save(validate: false)
     end
-    to_do_list = ToDoList.new(giver: users(:user_one), recipient: users(:user_two), active: nil)
-    assert_raises(ActiveRecord::NotNullViolation) do 
-      to_do_list.save(validate: false)
-    end
-    to_do_list = ToDoList.new(giver: users(:user_one), recipient: users(:user_two), active: true)
+    to_do_list = ToDoList.new(giver: users(:user_one), recipient: users(:user_two))
     assert to_do_list.save
   end
 
   test "all fields other than name are not nullable on the model" do
     to_do_list = ToDoList.new
     assert_not to_do_list.save
-    to_do_list = ToDoList.new(giver: nil, recipient: users(:user_two), active: true)
+    to_do_list = ToDoList.new(giver: nil, recipient: users(:user_two))
     assert_not to_do_list.save
-    to_do_list = ToDoList.new(giver: users(:user_one), recipient: nil, active: true)
+    to_do_list = ToDoList.new(giver: users(:user_one), recipient: nil)
     assert_not to_do_list.save
-    to_do_list = ToDoList.new(giver: users(:user_one), recipient: users(:user_two), active: nil)
-    assert_not to_do_list.save
-    to_do_list = ToDoList.new(giver: users(:user_one), recipient: users(:user_two), active: true)
+    to_do_list = ToDoList.new(giver: users(:user_one), recipient: users(:user_two))
     assert to_do_list.save
   end
 end
