e4570740625c38982ca2b0c5dbf485f85b6392cc
Michaela Healton <mshealton@gmail.com>
2023-05-12 18:03:56 -0700

    Refactor to do list visibility

--- app/controllers/to_do_lists_controller.rb
+++ app/controllers/to_do_lists_controller.rb
@@ -2,8 +2,10 @@ class ToDoListsController < ApplicationController
     before_action :authenticate_user!
 
     def show
-        @to_do_list = ToDoList.find_by(id: params[:id])
-        unless @to_do_list.present? && (current_user.id == @to_do_list.giver_id || current_user.id == @to_do_list.recipient_id)
+        @to_do_list = ToDoList.visible_to(current_user.id)
+                              .includes(:giver, :recipient)
+                              .find_by(id: params[:id])
+        unless @to_do_list
             render file: "#{Rails.root}/public/404.html", layout: false, status: :not_found
             return
         end
--- app/models/to_do_list.rb
+++ app/models/to_do_list.rb
@@ -3,4 +3,6 @@ class ToDoList < ApplicationRecord
     belongs_to :recipient, class_name: "User", inverse_of: :received_to_do_lists
     has_many :to_do_items
     has_one :to_do_list_invite
+
+    scope :visible_to, -> (user_id) { where(giver_id: user_id).or(where(recipient_id: user_id)) }
 end
