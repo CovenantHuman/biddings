601a00a383d46ff95632b1bcc4996936b44cf469
Michaela Healton <mshealton@gmail.com>
2023-05-18 16:49:07 -0700

    Fix to do item validation

--- app/models/to_do_item.rb
+++ app/models/to_do_item.rb
@@ -1,5 +1,5 @@
 class ToDoItem < ApplicationRecord
     belongs_to :to_do_list
     validates :name, presence: true
-    validates :completed, presence: true
+    validates :completed, inclusion: [true, false]
 end
