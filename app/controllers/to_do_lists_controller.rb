dd7b7cd59439d1e98c01e4a8c63fe7f92db46adc
Michaela Healton <mshealton@gmail.com>
2023-05-22 16:00:51 -0700

    Display completed and uncompleted to do items seperately

--- app/controllers/to_do_lists_controller.rb
+++ app/controllers/to_do_lists_controller.rb
@@ -5,7 +5,8 @@ class ToDoListsController < ApplicationController
         @to_do_list = ToDoList.visible_to(current_user.id)
                               .includes(:giver, :recipient)
                               .find_by(id: params[:id])
-        @to_do_items = ToDoItem.where(to_do_list_id: @to_do_list.id)
+        @to_do_items = ToDoItem.where(to_do_list_id: @to_do_list.id, completed: false)
+        @to_done_items = ToDoItem.where(to_do_list_id: @to_do_list.id, completed: true)
         unless @to_do_list
             render file: "#{Rails.root}/public/404.html", layout: false, status: :not_found
             return
